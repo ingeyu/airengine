03df48aa204d88c3e969ab270136848105671b54
bladeicefire <bladeicefire@163.com>
2012-11-08 11:14:58 +0000

    [Add]SmallObjectAllocator UserAllocator Stl

--- AirCommon/Inc/AirSmallObjectAllocator.h
+++ AirCommon/Inc/AirSmallObjectAllocator.h
@@ -22,5 +22,40 @@ namespace	Air{
 		U32		m_uiFreeEnd;
 		U32		m_uiMask;
 	};
+	template<class T>  
+	class user_defined_allocator : public std::allocator<T>  
+	{  
+	public:  
+		typedef std::allocator<T> base_type;  
+
+		// 必须要重新定义，否则容器如 list, set, map 使用时作用域只能到达 std::allocator   
+		template<class Other>  
+		struct rebind  
+		{  
+		   typedef user_defined_allocator<Other> other;  
+		};  
+
+		// 构造函数必须实现   
+		user_defined_allocator() {};  
+
+		user_defined_allocator(user_defined_allocator<T> const&) {};  
+
+		user_defined_allocator<T>& operator=(user_defined_allocator<T> const&) { return (*this); };  
+
+		// idiom: Coercion by Member Template   
+		template<class Other>  
+		user_defined_allocator(user_defined_allocator<Other> const&) {};  
+
+		// idiom: Coercion by Member Template   
+		template<class Other>  
+		user_defined_allocator<T>& operator=(user_defined_allocator<Other> const&) { return (*this); };  
+
+		// 内存的分配与释放可以实现为自定义的算法，替换函数体即可   
+		pointer allocate(size_type count) { return (pointer)__Alloc(count); };  
+	
+		void	deallocate(pointer ptr, size_type count) { __Free(ptr); };  
+	}; 
+	
+#define 	air_vector(T)	std::vector<T,user_defined_allocator<T> >
 }
 #endif // AirSmallObjectAllocator_h__
