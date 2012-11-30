a04dafd6fcb3ae24326df07ed22638d55d88e40d
bladeicefire <bladeicefire@163.com>
2012-11-30 10:49:54 +0000

    [Add]Common Shader header

---
+++ AirClient/Bin/Data/Shader/Common.h
@@ -0,0 +1,28 @@
+
+cbuffer cbPerFrame : register( b0 )
+{
+	float4	vTime;			///<	x=totaltime t=sin(x) z=cos(x) w= frac(time/(3600*24))
+	float4	vAmbient;
+	float4	vSunDir;		///<	xyz	=	direction	w	=	lighttype(0,1)
+	float4	vSunDiffuse;
+	float4	vMainCamPos;
+   
+}
+cbuffer cbPerCamera : register( b1 )
+{
+    	matrix  ViewProjection;
+	float4	vCameraDir;
+	float4	vCameraPosition;
+	float4	vCameraUp;
+	float4	vVP_NearFar;
+   
+}
+cbuffer	cbPerObject	:	register(b2)
+{
+	 matrix  World;
+}
+
+cbuffer cbPerSkeleton	:	register(b4)
+{
+	matrix	BoneMatrix[256];
+}
