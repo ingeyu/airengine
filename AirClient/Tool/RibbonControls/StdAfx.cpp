aedcfb960c2f5028649a34b3c336a1e9c92e2154
bladeicefire <bladeicefire@163.com>
2013-01-22 03:47:26 +0000

    Add Ribbon

---
+++ AirClient/Tool/RibbonControls/StdAfx.cpp
@@ -0,0 +1,11 @@
+// stdafx.cpp : source file that includes just the standard includes
+//	RibbonControls.pch will be the pre-compiled header
+//	stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+#pragma comment(lib,"AirCommon")
+#pragma comment(lib,"AirEngine")
+
+
+
